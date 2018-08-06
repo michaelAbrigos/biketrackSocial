7e752e166d7a1d1615840496fdc1374e67d55613
michaelAbrigos <michael.abrigos27@gmail.com>
2018-08-06 20:55:41 +0800

    hisory feature finished

--- public/js/history.js
+++ public/js/history.js
@@ -1,12 +1,12 @@
 var map;
-
+var markerArray = [];
 function initMap() {
 	var startplace = {lat: 14.5176, lng: 121.0509};
     var map = new google.maps.Map(document.getElementById('map'), {
       center: startplace,
       zoom: 13
     });
-}
+
 
 var submit = document.getElementById('submit');
 
@@ -31,11 +31,22 @@ submit.addEventListener('click',function(e){
         datatype: 'json',
         success: function(data){
             console.log(data);
+            clearMarkers();
+            $.each(data,function(index){
+                console.log(data[index].latitude);
+                var myLocation = {lat: data[index].latitude , lng: data[index].longitude};
+                var marker = new google.maps.Marker({position: myLocation, map: map});
+                markerArray.push(marker);
+                marker.setMap(map);
+            });
+            
         },error: function(data){
             console.log(data);
         }
     })
 });
+}
+
 
 function setMapOnAll(map) {
     for (var i = 0; i < markerArray.length; i++) {
